87009af4aafe8de7285e109537674246d9c9c4d1
Beni Zuñe <webmaster.grupocafimi@gmail.com>
2025-03-17 15:07:51 -0500

    Index

--- index.php
+++ index.php
@@ -599,12 +599,12 @@
       <!-- Main Footer -->
       <footer class="main-footer">
         <strong
-          >Copyright &copy; 2014-2021
-          <a href="https://adminlte.io">AdminLTE.io</a>.</strong
+          >Copyright &copy; 2025
+          <a href="https://adminlte.io">Grupo Cafimi</a>.</strong
         >
         All rights reserved.
         <div class="float-right d-none d-sm-inline-block">
-          <b>Version</b> 3.2.0
+          <b>Version</b> 0.1
         </div>
       </footer>
     </div>
