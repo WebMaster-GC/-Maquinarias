5718901564667c80b8d897ddda42d2c9e60bdfd5
Beni Zuñe <webmaster.grupocafimi@gmail.com>
2025-03-20 10:46:15 -0500

    Plugins

---
+++ pages/empresa/mant_empresa.php
@@ -0,0 +1,70 @@
+<div class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1 class="m-0"><b>EMPRESAS</b></h1>
+            </div><!-- /.col -->
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="index.php">Inicio</a></li>
+                    <li class="breadcrumb-item active">Empresas</li>
+                </ol>
+            </div><!-- /.col -->
+        </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+</div>
+
+<div class="row container-fluid">
+    <div class="col-lg-12">
+        <div class="card ">
+            <div class="card-header">
+                <h3 class="card-title"><b>Listado De Empresas</b></h3>
+                <button class="btn btn-danger btn-sm float-right" onclick="AbrirModalRegistroEmpresa()"> <b
+                        class="fas fa-plus"></b> Nueva empresa
+                </button>
+            </div>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-12 table-responsive">
+                        <table id="tabla_empresa" class="display" width="100%">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Logo</th>
+                                    <th>Razon Social</th>
+                                    <th>RUC</th>
+                                    <th>Email</th>
+                                    <th>Accion</th>
+                                </tr>
+                            </thead>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
